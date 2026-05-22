3784051e984f3c24688b712156e3272ef8e9ba70
Sandy Kurniawan <sandy.kurniawan@dataon.com>
2026-05-22 17:23:28 +0700

    feat: update function

--- hello_world.go
+++ hello_world.go
@@ -1,5 +1,27 @@
 package hello_world
 
+import (
+	"strconv"
+	"fmt"
+)
+
+type UserInformation struct {
+	name, address string
+	age int
+}
+
+func DisplayUser(name string, address string, age int) string {
+	return "Hello " + name + " " + address + " age " + strconv.Itoa(age)
+}
+
 func HelloWorld() string {
-	return "Hello World"
+	user := UserInformation{
+		name: "Jane Doe",
+		address: "Bintaro",
+		age: 25,
+	}
+
+	displayUser := DisplayUser
+
+	return displayUser(user.name, user.address, user.age)
 }
